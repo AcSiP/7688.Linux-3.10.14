e80c83e958829551015792b5242398ead83fc014
Sugar <sugar@mqmaker.com>
2015-08-16 17:58:22 +0800

    sound: fixup compile errors

--- include/sound/pcm.h
+++ include/sound/pcm.h
@@ -126,6 +126,8 @@ struct snd_pcm_ops {
 #define SNDRV_PCM_RATE_96000		(1<<10)		/* 96000Hz */
 #define SNDRV_PCM_RATE_176400		(1<<11)		/* 176400Hz */
 #define SNDRV_PCM_RATE_192000		(1<<12)		/* 192000Hz */
+#define SNDRV_PCM_RATE_12000		(1<<13)		/* 12000Hz */
+#define SNDRV_PCM_RATE_24000		(1<<14)		/* 24000Hz */
 
 #define SNDRV_PCM_RATE_CONTINUOUS	(1<<30)		/* continuous range */
 #define SNDRV_PCM_RATE_KNOT		(1<<31)		/* supports more non-continuos rates */
